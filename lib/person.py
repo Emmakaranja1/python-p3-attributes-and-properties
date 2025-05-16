e5c012fdadb395c74093399444effd17ea636f08
Emmakaranja1 <karanjaemmak@gmail.com>
2025-05-16 18:57:13 +0300

    Add name and job properties with validation to Person class

--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,33 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
+    def __init__(self,  name="Person", job="Admin"):
+        self.name = name
+        if hasattr(self, "_name"):  # Only set job if name is valid
+            self.job = job
+
+     # Name property
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+
+    # Job property
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+    job = property(get_job, set_job)
+
     pass
