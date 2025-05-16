63bc162028339615b58a01920fbfba633f64aacc
Emmakaranja1 <karanjaemmak@gmail.com>
2025-05-16 18:59:14 +0300

    Implement name and breed properties with validation in class constructor

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,32 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
+    def __init__(self, name="Dog", breed="Mutt"):
+        self.name = name
+        if hasattr(self, "_name"):  # Only set breed if name is valid
+            self.breed = breed
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+    breed = property(get_breed, set_breed)
+
     pass
